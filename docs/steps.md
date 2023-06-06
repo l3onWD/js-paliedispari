e44898782348d141e8ff3e52d1ff02f60bfa606b
l3on <l3onwebdev@gmail.com>
2023-06-06 15:26:49 +0200

    added steps file with palindrome logic

---
+++ docs/steps.md
@@ -0,0 +1,21 @@
+# Pali e Dispari
+
+<br>
+
+## Steps Palindroma:
+
+- Recupero il bottone
+- **QUANDO** _"ho cliccato il bottone"_
+    - Recupero la parola inserita
+    - Creo un messaggio di insuccesso
+    - _"Verifico se una parola è palindroma"_
+    - **SE** _"la parola è palindroma"_
+        - Aggiorno il messaggio con il successo
+    - Mostro il risultato
+- **???** _"Verifico se una parola è palindroma"_
+    - **FINCHE** _"Ho caratteri nella parola, contando all'indietro"_
+        - Salvo il carattere corrente
+    - **SE** _"la parola inserita è uguale a quella invertita"_
+        - La parola è palindroma
+        - **FINE**
+    - La parola non è palindroma
--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-/* -----------------------------------------
-! DEBUG
--------------------------------------------*/
-console.log('----------- DEBUG -----------');
+/*
+Palidroma
+    - Chiedere all’utente di inserire una parola
+    - Creare una funzione per capire se la parola inserita è palindroma
 
-console.log('JS OK!');// Check if JS is linked to the page
+Pari e Dispari
+    - L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    - Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    - Sommiamo i due numeri
+    - Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+    - Dichiariamo chi ha vinto.
 
-
+Consigli del giorno
+    1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+    2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+*/
 /* -----------------------------------------
 * INIT
 -------------------------------------------*/
