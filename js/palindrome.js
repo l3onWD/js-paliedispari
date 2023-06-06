4d0def7cc6c7f304a442bb6730f4750fd6f04525
l3on <l3onwebdev@gmail.com>
2023-06-06 16:41:33 +0200

    created 2 separated scripts

---
+++ js/palindrome.js
@@ -0,0 +1,24 @@
+/*
+Palidroma
+    - Chiedere all’utente di inserire una parola
+    - Creare una funzione per capire se la parola inserita è palindroma
+*/
+/* -----------------------------------------
+* INIT
+-------------------------------------------*/
+console.log('----------- INIT -----------');
+
+
+//*** DOM ELEMENTS ***//
+
+
+
+/* -----------------------------------------
+* LOGIC
+-------------------------------------------*/
+console.log('----------- LOGIC -----------');
+
+
+
+
+console.log('----------- DONE -----------');
